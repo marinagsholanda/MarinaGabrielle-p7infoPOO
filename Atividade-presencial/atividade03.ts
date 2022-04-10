8201ffcd01800cba00edfc4ece7b4cf824daafeb
Marina <marinagabrielle274@gmail.com>
2022-04-09 23:40:43 -0300

    atividade03

---
+++ Atividade-presencial/atividade03.ts
@@ -0,0 +1,77 @@
+class Stack{
+    stack: number[] = []
+    topo: number = -1
+    maxStack = 4
+
+    isEmpty(): number{
+        if(this.topo == -1){
+            return 1
+        }else{
+            return 0
+        }
+    }
+    
+    isFull(): number{
+        if(this.topo == this.maxStack){
+            return 1
+        }else{
+            return 0
+        }
+    }
+    
+    push(num: number): void{
+        if(this.isFull() == 1){
+            console.log('PILHA CHEIA!')
+        }else{
+            this.topo++
+            this.stack[this.topo] = num
+            console.log('ELEMENTO '+num+' INSERIDO!')
+        }
+    }
+    
+    pop(): number | void{
+        if(this.isEmpty() == 1){
+            console.log('PILHA VAZIA')
+        }else{
+            let aux = this.stack[this.topo]
+            console.log('ELEMENTO '+aux+' RETIRADO!')
+            this.topo--
+            return aux
+        }
+    }
+    
+    view(): void{
+        if(this.isEmpty() == 1){
+            console.log('PILHA VAZIA!')
+         }else{
+            for(let n = 0; n <= this.topo; n++){
+                console.log('PILHA POSSUE O ELEMENTO '+this.stack[n])    
+            }
+        }
+    }
+}
+
+let pilha: Stack = new Stack()
+
+
+pilha.push(1)
+pilha.push(2)
+pilha.push(3)
+pilha.push(4)
+console.log('-------------------------')
+
+pilha.view()
+console.log('-------------------------')
+
+pilha.pop()
+pilha.pop()
+console.log('-------------------------')
+
+pilha.view()
+console.log('-------------------------')
+
+pilha.pop()
+pilha.pop()
+console.log('-------------------------')
+
+pilha.view()
