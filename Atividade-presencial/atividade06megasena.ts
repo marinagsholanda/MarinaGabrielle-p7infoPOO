5d3f257e02d88f7165d96fa32235513a68a73326
Marina <marinagabrielle274@gmail.com>
2022-05-06 02:18:51 -0300

    atividade06megasena

---
+++ Atividade-presencial/atividade06megasena.ts
@@ -0,0 +1,14 @@
+function gerandoRandom(){
+    var max = 6
+    var random = []
+    for(var n=0; n<max; n++){
+        var num = Math.floor(Math.random()*60);
+        if(random.indexOf(num) == -1){
+            random.push(num);
+        }
+        else
+        n--;
+    }
+    console.log(random)
+}
+console.log(gerandoRandom());
